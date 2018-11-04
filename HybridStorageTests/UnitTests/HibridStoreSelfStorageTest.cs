9bd30787229ccc55c49dfc3ba5984d7a8d2ed62b
Christian Rodriguez <christianarg@outlook.com>
2018-11-04 01:36:25 +0100

    Algunas mejoras test SelfStoredContent

--- HybridStorageTests/UnitTests/HibridStoreSelfStorageTest.cs
+++ HybridStorageTests/UnitTests/HibridStoreSelfStorageTest.cs
@@ -1,7 +1,8 @@
-﻿using System;
+﻿using HybridStorage;
+using HybridStorageTests.TestModel.AdvancedTestModel;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
-using HybridStorage;
+using System.Collections.Generic;
 
 namespace HybridStorageTests.UnitTests
 {
@@ -10,16 +11,17 @@ namespace HybridStorageTests.UnitTests
     {
         public string Id { get; set; }
         public string Language { get; set; }
-
+        public List<Field> Fields { get; set; }
         [JsonIgnore] // De momento es necesario
         public string SelfStorage { get; set; }
     }
+
 
     [TestClass]
     public class HibridStoreSelfStorageTest : HybridStoreTestBase
     {
         SelfStoredContent content;
- 
+
         const string testId = "theId";
         const string testLanguage = "es-ES";
 
@@ -33,11 +35,23 @@ namespace HybridStorageTests.UnitTests
         [TestCategory(TestConstants.UnitTest)]
         public void LoadSelfStored()
         {
-            content.SelfStorage = JsonConvert.SerializeObject(new { Id = testId, Language = testLanguage });
+            // ARRANGE
+            content.SelfStorage = JsonConvert.SerializeObject(new
+            {
+                Id = testId,
+                Language = testLanguage,
+                Fields = new List<Field> { new Field { Name = "FieldName", Value = "FieldValue" } }
+            });
+
+            // ACT
             modelStore.LoadStoredModels(content);
 
+            // ASSERT
             Assert.AreEqual(testId, content.Id);
             Assert.AreEqual(testLanguage, content.Language);
+            Assert.AreEqual("FieldName", content.Fields[0].Name);
+            Assert.AreEqual("FieldValue", content.Fields[0].Value);
+
         }
 
 
@@ -45,9 +59,18 @@ namespace HybridStorageTests.UnitTests
         [TestCategory(TestConstants.UnitTest)]
         public void StoreSelfStored()
         {
-            content = new SelfStoredContent() { Id = testId, Language = testLanguage };
+            // ARRANGE
+            content = new SelfStoredContent
+            {
+                Id = testId,
+                Language = testLanguage,
+                Fields = new List<Field> { new Field { Name = "FieldName", Value = "FieldValue" } }
+            };
+
+            // ACT
             modelStore.StoreStoredModels(content);
 
+            // ASSERT
             Assert.AreEqual(serializer.Serialize(content), content.SelfStorage);
         }
     }
